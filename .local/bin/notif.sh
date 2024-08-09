154c37b3e162d8610e56a24cdf305b8c1eaff049
vixel.view_ <145457683+pyranix@users.noreply.github.com>
2024-08-10 03:31:33 +0700

    Delete .local/bin/notif.sh

--- .local/bin/notif.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$DUNST_BODY" != "^Volume" ] && [ "$DUNST_BODY" != "^Brightness" ]; then
-    pw-play ~/.local/share/sounds/tuturu.mp3
-elif [ "$DUNST_BODY" = "^Battery low" ]; then
-    pw-play ~/.local/share/sounds/hungaaa.mp3
-elif [ "$DUNST_URGENCY" = "critical" ]; then
-    pw-play ~/.local/share/sounds/kakegurui_subarashii.mp3
-fi
