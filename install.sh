26556b7f6df1c6f7e495c36a58253970530c1838
vixel.view_ <2300018265@webmail.uad.ac.id>
2025-01-21 05:37:32 +0700

    update: script: install.sh
    Missing Package

--- install.sh
+++ install.sh
@@ -24,10 +24,47 @@ BOLD="\033[1m"
 VOID_PACKAGES=(
     xclip powertop xfce4-notifyd pulseaudio bluez blueman rofi pamixer
     NetworkManager alacritty git curl neovim xfce4-settings qtile xorg-minimal
+    xorg-input-drivers xorg-fonts xorg-video-drivers xorg-server xsettingsd
+    dconf-editor dconf rsync wget aria2 dunst python3 feh gtk+ gtk+3 gtk4
+    nano xinit xsetroot dbus elogind gcc gcc-multilib thunar-volman
+    thunar-archive-plugin thunar-media-tags-plugin pipewire pavucontrol
+    starship psutils acpi acpica-utils acpid dhcpcd-gtk ImageMagick pfetch
+    htop exa openssh openssl xdg-user-dirs xdg-user-dirs-gtk picom gnupg2
+    mpv nwg-launchers nwg-look linux-firmware-intel intel-gmmlib
+    intel-gpu-tools intel-media-driver intel-ucode intel-video-accel
+    vulkan-loader android-file-transfer-linux android-tools android-udev-rules
+    libvirt libvirt-glib libvirt-python3 gvfs udiskie udisks2 brightnessctl
+    xdotool apparmor libselinux rpm rpmextract lxappearance lxappearance-obconf
+    xfce4-power-manager xfce-polkit polkit-elogind maim viewnior nodeenv
+    nodejs xdg-desktop-portal xdg-desktop-portal-kde xdg-desktop-portal-wlr
+    xdg-desktop-portal-gnome xdg-desktop-portal-gtk zip unzip tar 7zip bzip2
+    zstd lz4 xz libXft-devel libXinerama-devel make virt-manager fish-shell
+    pasystray network-manager-applet
 )
 ARCH_PACKAGES=(
-    xclip powertop libnotify xfce4-notifyd bluez blueman rofi
+    plymouth schedtool modprobe-db update-grub xclip powertop libnotify
+    xfce4-notifyd bluez bluez-plugins bluez-cups cups blueman rofi
     networkmanager alacritty git curl neovim xfce4-settings qtile xorg-xinput
+    xorg-drivers xorg-fonts xorg-xvidtune xorg-server xsettingsd dconf
+    dconf-editor rsync wget aria2 dunst python feh gtk3 gtk4 gtk2 nano
+    xorg-xinit xorg-xsetroot gcc thunar thunar-archive-plugin
+    thunar-media-tags-plugin thunar-volman pavucontrol psutils python-psutil
+    acpi acpica acpid imagemagick htop exa fzf expac openssh openssl
+    xdg-desktop-portal xdg-desktop-portal-wlr xdg-desktop-portal-gtk
+    xdg-desktop-portal-xapp xdg-user-dirs xdg-user-dirs-gtk xdg-utils picom
+    gnupg mpv linux-firmware intel-media-driver intel-gmmlib intel-gpu-tools
+    intel-ucode intel-media-sdk xf86-input-libinput xf86-input-synaptics
+    xf86-video-intel xf86-video-fbdev xf86-video-vesa xf86-input-evdev
+    vulkan-intel vulkan-icd-loader vulkan-mesa-layers vulkan-tools
+    vulkan-utility-libraries vulkan-virtio vulkan-validation-layers
+    lib32-vulkan-icd-loader lib32-vulkan-intel lib32-vulkan-mesa-layers
+    lib32-vulkan-validation-layers lib32-vulkan-virtio android-file-transfer
+    android-tools android-udev gvfs gvfs-afc gvfs-goa gvfs-google gvfs-gphoto2
+    gvfs-mtp gvfs-nfs gvfs-smb udiskie udisks2 brightnessctl polkit xdotool
+    apparmor lxappearance lxappearance-obconf xfce4-power-manager maim
+    viewnior nodejs gedit gedit-plugins zip unzip tar p7zip unrar bzip2
+    zstd lz4 xz libxft libxinerama make cmake fish pasystray
+    network-manager-applet gamescope gamemode jq
 )
 
 # Logging with enhanced visuals
